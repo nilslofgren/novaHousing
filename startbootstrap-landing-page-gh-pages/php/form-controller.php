4d2cfae89d5f94352318567394cb949f9638483b
Nils Lofgren <nlofgren@villanova.edu>
2018-04-27 14:31:59 -0400

    Add Form
    Fix navbar links to appropriate pages, add styling for fom

---
+++ startbootstrap-landing-page-gh-pages/php/form-controller.php
@@ -0,0 +1,11 @@
+<?php
+
+echo 'Hello World!'
+//  $user_name = htmlspecialchars($_POST['user_name']);
+//  $user_mail = htmlspecialchars($_POST['user_mail']);
+//  $user_message = htmlspecialchars($_POST['user_message']);
+//
+// echo $user_name, 'with the email: ', $user_mail, 'says: ', $user_message;
+
+
+ ?>
