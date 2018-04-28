2e7032a89d01d70b646538b5939890d9007be1c4
Nils Lofgren <nlofgren@villanova.edu>
2018-04-28 13:35:39 -0400

    update php

--- startbootstrap-landing-page-gh-pages/php/form-controller.php
+++ startbootstrap-landing-page-gh-pages/php/form-controller.php
@@ -8,18 +8,3 @@ This is your username: <?php echo $_GET['user_name']; ?>
 
 </body>
 </html>
-
-<?php
-$servername = "CSDB-Remote.sql";
-$username = "nlofgren";
-$password = "fL@01509628";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-?>
