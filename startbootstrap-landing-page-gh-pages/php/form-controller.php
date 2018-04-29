1792e5a37ed8de703e006e05b5a0859fbca93a96
ZonkRT <33356792+ZonkRT@users.noreply.github.com>
2018-04-29 15:08:32 -0400

    Add files via upload

--- startbootstrap-landing-page-gh-pages/php/form-controller.php
+++ startbootstrap-landing-page-gh-pages/php/form-controller.php
@@ -1,10 +1,32 @@
-<html>
-<body>
+<?php
+$servername = "den1.mysql6.gear.host";
+$username = "subletparadise1";
+$password = "P@ssw0rd";
+$dbname = "subletparadise1";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$Address = $_POST['user_address'];
+$Name = $_POST['user_name'];
+$Owner = $_POST['owner_name'];
+$Email = $_POST['user_mail'];
+$Phone = $_POST['user_tel'];
+$Message = $_POST['user_message'];
 
-Success! Your input has been recorded!
+$sql = "INSERT INTO Properties (propAddress, propName, ownName, propEmail, propTel, propMessage)
+VALUES ('$Address', '$Name', '$Owner', '$Email', '$Phone', '$Message')";
 
-This is your username: <?php echo $_POST['user_name']; ?>
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
-</body>
-</html>
+$conn->close();
+
+header('Location: /subletparadise/thanks.html');
+?>
